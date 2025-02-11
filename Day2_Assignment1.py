ae312543935ebcc8f76f64e899ce3192608ad7ca
Khukha <106427784+Khukha@users.noreply.github.com>
2025-02-11 14:34:48 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,14 @@
+#Calculate the multiplication and sum of two numbers
+a=4
+b=8
+c=a*b
+d=a+b
+print("multiplication of a&b is:",c)
+print("Sum of a&b is:",d)
+
+
+#output
+
+#multiplication of a&b is: 32
+#Sum of a&b is: 12
+
---
+++ Day2_Assignment2.py
@@ -0,0 +1,8 @@
+#Declare two variables and print that which variable is largest using ternary operators
+a=5
+b=6
+largest = a if a>b else b
+print("The largest value is:",largest)
+
+#output: The largest value is:6
+ 
