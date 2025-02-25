ac72c9a5865aa149ae3953989a04179dda51c594
Khukha <106427784+Khukha@users.noreply.github.com>
2025-02-25 15:52:01 +0530

    Add files via upload

---
+++ Day12_Assignment1.py
@@ -0,0 +1,12 @@
+# Define the tuple
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+
+# Use the count() method to count occurrences of 4
+count_of_4 = tuplex.count(4)
+
+# Print the result
+print("The number of times 4 appears in the tuple:", count_of_4)
+
+'''
+#Output= The number of times 4 appears in the tuple: 3
+'''
---
+++ Day12_Assignment2.py
@@ -0,0 +1,13 @@
+# Define the input list
+listx = [5, 10, 7, 4, 15, 3]
+
+# Convert the list to a tuple using the tuple() function
+tuplex = tuple(listx)
+
+# Print the resulting tuple
+print(tuplex)
+
+'''
+#Output
+(5, 10, 7, 4, 15, 3)
+'''
